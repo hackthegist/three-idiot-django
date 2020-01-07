8a0069cb77d4def5faa21546175ecda54f03ac24
YoonJaeKim <rladbswo1124@gmail.com>
2020-01-07 15:42:06 +0900

    movie research
    Former-commit-id: 2fd193d2f0abffdd28da7b5c4198b72eab9968c4

--- final_project_django/movies/serializers.py
+++ final_project_django/movies/serializers.py
@@ -4,12 +4,12 @@ from .models import Genre, Movie, Review
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ('id', 'genre',)
+        fields = ('id', 'name',)
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'summary', 'genresNm', 'prdtYear', 'openDt', 'showTm', 'nationNm', 'actorsNm', 'watchGradeNm', 'companyNmDict', 'link', 'image', 'userRating', 'audiAcc', 'thumbsNm', 'thumbsImage',)
+        fields = ('id', 'title', 'summary', 'directorNm', 'genresCds', 'prdtYear', 'openDt', 'showTm', 'nationNm', 'actorsNm', 'watchGradeNm', 'companyNmDict', 'link', 'image', 'userRating', 'audiAcc', 'thumbsNm', 'thumbsImage',)
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
